81785539634c7c8aca7e05c637fe3090bc9e06dc
Madhurima <madhurimasanyal338@gmail.com>
2026-05-21 18:48:16 +0530

    Testing webhook automation

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "My first CI/CD app is running!"
+    return "Webhook automation is successful!!"
 
 app.run(host='0.0.0.0', port=5000)
